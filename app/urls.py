aa934cecd8d79983cec4800ca74040c6242fdc18
ahmadkhan5346 <khansarfraz5346@gmail.com>
2022-11-29 20:52:33 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from app import views
+
+
+urlpatterns = [
+    path('', views.ProductView.as_view(), name='home'),
+    path('product-detail/<int:pk>', views.ProductDetailView.as_view, name='product-detail'),
+
+
+    # path('', views.home),
+    # path('cart/', views.add_to_cart, name='add-to-cart'),
+    # path('buy/', views.buy_now, name='buy-now'),
+    # path('profile/', views.profile, name='profile'),
+    # path('address/', views.address, name='address'),
+    # path('orders/', views.orders, name='orders'),
+    # path('changepassword/', views.change_password, name='changepassword'),
+    # path('mobile/', views.mobile, name='mobile'),
+    # path('login/', views.login, name='login'),
+    # path('registration/', views.customerregistration, name='customerregistration'),
+    # path('checkout/', views.checkout, name='checkout'),
+]
